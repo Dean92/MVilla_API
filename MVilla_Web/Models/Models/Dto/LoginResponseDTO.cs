ea9f9a2995d4f960a3999a99dca611be0ff4e9f1
Dean McCoy <dean92@gmail.com>
2023-03-10 16:50:11 -0600

    Create views for Login and Register.

---
+++ MVilla_Web/Models/Models/Dto/LoginResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿
+using MVilla_Web.Models.Models.Dto;
+
+namespace MVilla_Web.Models.Dto
+{
+	public class LoginResponseDTO
+	{
+		public UserDTO User { get; set; }
+		public string Token { get; set; }
+	}
+}
---
+++ MVilla_Web/Models/Models/Dto/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MVilla_Web.Models.Models.Dto
+{
+	public class UserDTO
+	{
+		public int Id { get; set; }
+		public string UserName { get; set; }
+		public string Name { get; set; }
+		public string Password { get; set; }
+		public string Role { get; set; }
+	}
+}
